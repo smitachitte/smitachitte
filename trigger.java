8ca2b865996854f09de58ea8d006e860359522ea
smitachitte <52373350+smitachitte@users.noreply.github.com>
2019-07-17 17:21:26 +0530

    Update trigger.java

--- trigger.java
+++ trigger.java
@@ -3,6 +3,7 @@ public class trigger {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello smita chitte, World");
+         System.out.println("how r u");
     }
 
 }
